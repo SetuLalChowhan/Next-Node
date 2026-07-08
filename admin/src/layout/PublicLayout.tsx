7dabb171b74546ca38b54f8e4c18273e26f3a7aa
Setulal Chowhan <setulalchowhan@gmail.com>
2026-07-08 12:07:02 +0600

    feat: add PublicLayout component for unauthenticated routes

--- admin/src/layout/PublicLayout.tsx
+++ admin/src/layout/PublicLayout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom"
 const PublicLayout: React.FC = () => {
   return (
     <div className="min-h-screen w-full bg-[#f9fafb] text-foreground flex flex-col justify-center items-center">
-      <div className="w-full max-w-md p-4 sm:p-6 flex flex-col justify-center">
+      <div className="w-full max-w-[800px] p-4 sm:p-6 flex flex-col justify-center">
         <Outlet />
       </div>
     </div>
